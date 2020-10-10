eb64a06c781611286ea2f34247868e9d29f2d131
shaw8wit <shaw8wit@gmail.com>
2020-10-10 16:55:40 +0530

    finished

--- 5_neural_networks/traffic/traffic.py
+++ 5_neural_networks/traffic/traffic.py
@@ -1,7 +1,8 @@
-import cv2
-import numpy as np
 import os
 import sys
+import cv2
+import glob
+import numpy as np
 import tensorflow as tf
 
 from sklearn.model_selection import train_test_split
@@ -75,7 +76,24 @@ def get_model():
     `input_shape` of the first layer is `(IMG_WIDTH, IMG_HEIGHT, 3)`.
     The output layer should have `NUM_CATEGORIES` units, one for each category.
     """
-    raise NotImplementedError
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Conv2D(32, (3, 3), activation="relu",
+                               input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        tf.keras.layers.Conv2D(64, (3, 3), activation="relu"),
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(128, activation="relu"),
+        tf.keras.layers.Dense(64, activation="relu"),
+        tf.keras.layers.Dropout(0.33),
+        tf.keras.layers.Dense(NUM_CATEGORIES, activation="softmax")
+    ])
+    model.compile(
+        optimizer="adam",
+        loss="categorical_crossentropy",
+        metrics=["accuracy"]
+    )
+    return model
 
 
 if __name__ == "__main__":
